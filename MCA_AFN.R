a99f9d6011e67f49f74e7420c71cc2ac1552b355
Amy Neal <amyfneal87@gmail.com>
2026-04-14 16:49:44 -0400

    Adjusting composites and creating visualizations

--- MCA_AFN.R
+++ MCA_AFN.R
@@ -6,6 +6,11 @@ library(tidyverse) #data cleaning and manipulation
 library(sjlabelled) #get variable labels 
 library(psych) #descriptive statistics
 library(FactoMineR) #MCA function
+library(paletteer) #color palette 'MoMAColors::Klein'
+
+
+# LAST LEFT OFF: composite score of positive and negative affect line 144
+# reverse code negative items and take composite 
 
 
 # Read in and clean data -------------------------------------------
@@ -47,6 +52,25 @@ study2_clean <- study2 %>%
 # of use across all 10 platforms in the survey 
 study2_clean <- study2_clean %>%
   mutate(SMS_freq_avg = rowMeans(pick(SMS_plat_freq_1:SMS_plat_freq_10), na.rm = TRUE))
+
+#factor demographic variables 
+study2_clean <- study2_clean %>%
+  mutate(
+    p_gender = factor(p_gender, labels = c('Agender',
+                                           'Man',
+                                           'Woman',
+                                           'Non-binary')),
+    p_trans = factor(p_trans, labels = c('Transgender',
+                                         'Cisgender')),
+    p_sexual_orientation = factor(p_sexual_orientation, labels = c('Asexual',
+                                                                   'Bisexual',
+                                                                   'Heterosexual',
+                                                                   'Lesbian or Gay',
+                                                                   'Pansexual',
+                                                                   'Not listed')),
+    rel_distance = factor(rel_distance, labels = c('Long distance',
+                                                   'No long distance'))
+  )
 
 # MCA Analysis ----------------------------------------------------------------
 
@@ -117,6 +141,8 @@ study2_clean <- study2_clean %>%
     positive_affect = rowMeans(pick(pos_neg_affect_1:pos_neg_affect_3), na.rm = TRUE),
     # negative affect total 
     negative_affect = rowMeans(pick(pos_neg_affect_4:pos_neg_affect_6), na.rm = TRUE),
+    #total affect, high = positive 
+    pos_neg_affect_4R = 
     # self esteem
     est_connect_1R = 8 - est_connect_1,
     est_connect_4R = 8 - est_connect_4,
@@ -130,22 +156,38 @@ study2_clean <- study2_clean %>%
 study2_clean <- study2_clean %>%
   mutate(
     # satisfaction with life scale 
-    SWLS_bin = cut(SWLS_avg, breaks = c(-Inf, 2, 5, 7), labels = c('1','2','3')),
+    SWLS_bin = cut(SWLS_avg, breaks = c(0, 2.33, 4.66, 7), labels = c('low','mid','high')),
     # online relationship visibility
-    SMS_visibility_bin = cut(SMS_visibility_avg, breaks = c(0, 2, 5, 7), labels = c('1','2','3')),
+    SMS_visibility_bin = cut(SMS_visibility_avg, breaks = c(0, 2.33, 4.66, 7), labels = c('low','mid','high')),
     # IMS satisfaction 
-    IMS_sat_bin = cut(IMS_sat, breaks = c(0, 3, 6, 9), labels = c('1','2','3')),
+    IMS_sat_bin = cut(IMS_sat, breaks = c(0, 3, 6, 9), labels = c('low','mid','high')),
     # IMS commitment
-    IMS_com_bin = cut(IMS_sat, breaks = c(0, 3, 6, 9), labels = c('1','2','3')),
+    IMS_com_bin = cut(IMS_sat, breaks = c(0, 3, 6, 9), labels = c('low','mid','high')),
+    #IMS total 
+    IMS_total_bin = cut(IMS_total, breaks = c(0, 3, 6, 9), labels = c('low','mid','high')),
     # positive affect 
-    positive_affect_bin = cut(positive_affect, breaks = c(0, 2, 5, 7), labels = c('1','2','3')),
+    positive_affect_bin = cut(positive_affect, breaks = c(0, 2.33, 4.66, 7), labels = c('low','mid','high')),
     # negative affect
-    negative_affect_bin = cut(negative_affect, breaks = c(0,2,5,7), labels = c('1','2','3')),
+    negative_affect_bin = cut(negative_affect, breaks = c(0,2.33,4.66,7), labels = c('low','mid','high')),
     #self esteem 
-    esteem_bin = cut(esteem_avg, breaks = c(0, 2, 5, 7), labels = c('1','2','3')),
+    esteem_bin = cut(esteem_avg, breaks = c(0, 2.33, 4.66, 7), labels = c('low','mid','high')),
     #relationship understanding
-    rel_understanding_bin = cut(rel_understanding_avg, breaks = c(0, 2, 3, 5), labels = c('1','2','3'))
+    rel_understanding_bin = cut(rel_understanding_avg, breaks = c(0, 1.66, 3.33, 5), labels = c('low','mid','high'))
   )
+
+#correlation between IMS satisfaction and commitment scores 
+cor(study2_clean$IMS_sat, study2_clean$IMS_com) # r = .80 
+
+#visual representation of bin distributions 
+plot(study2_clean$SWLS_bin) #majority of respondents in bin 2
+plot(study2_clean$SMS_visibility_bin) #majority of respondents in bin 2
+plot(study2_clean$IMS_sat_bin) #majority of respondents in bin 3
+plot(study2_clean$IMS_com_bin) #majority of respondents in bin 3
+plot(study2_clean$IMS_total_bin) #majority of respondents in bin 3
+plot(study2_clean$positive_affect_bin) #majority of respondents in bin 2
+plot(study2_clean$negative_affect_bin) #majority of respondents in bin 2, higher proportion in bin 1
+plot(study2_clean$esteem_bin) #majority of respondents in bins 2 and 3
+plot(study2_clean$rel_understanding_bin) #majority of respondents in bin 3
 
 # isolate variables of interest for binning model 
 mca_bin_pred <- study2_clean %>%
@@ -156,8 +198,7 @@ mca_bin_pred <- study2_clean %>%
     rel_distance, #relationship long distance (y/n)
     SWLS_bin, #satisfaction with life average 
     SMS_visibility_bin, #online relationship visibility average
-    IMS_sat_bin, #investment model satisfaction scale 
-    IMS_com_bin, #investment model commitment scale 
+    IMS_total_bin, #investment model composite score
     positive_affect_bin, #positive affect/mood 
     negative_affect_bin, #negative affect/mood 
     esteem_bin, #self-esteem score 
@@ -165,23 +206,17 @@ mca_bin_pred <- study2_clean %>%
   ) %>%
   na.omit()
 
-mca_bin_pred <- mca_bin_pred %>%
-  mutate(
-    p_gender = as.factor(p_gender),
-    p_trans = as.factor(p_trans),
-    p_sexual_orientation = as.factor(p_sexual_orientation),
-    rel_distance = as.factor(rel_distance)
-  )
-
 ### build MCA model with new predictor variables -------------------------
 
-mca_bin_model <- MCA(mca_bin_pred, graph = FALSE)
-summary(mca_bin_model)
+mca_bin_model <- MCA(mca_bin_pred, graph = TRUE)
+summary(mca_bin_model, nbelements = Inf)
+fviz_mca_var(mca_bin_model)
 
 # scree plot of dimensions 
 fviz_screeplot(mca_bin_model)
 # higher percentage of variance explained by binned variables than original variables 
 # scree plot suggests 3 or 4 dimensions are appropriate 
+# model with IMS composite score suggests 3 dimensions are appropriate 
 
 # see variable contributions for top 4 dimensions 
 for (dim in 1:4) {
@@ -194,3 +229,75 @@ for (dim in 1:4) {
   #display plots 
   print(p)
 }
+
+# additional visualizations 
+
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'p_gender',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(3,4)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+#ellipse shows confidence intervals. If ellipses do not overlap, groups are significantly different
+
+
+dimdesc(mca_bin_model, axes = c(1:4))
+
+#variable importance dimensions 1 and 2
+fviz_mca_var(mca_bin_model, choice = 'var', axes = c(1,2))
+# most important variables are ims score, positive affect, relationship understanding, 
+# satisfaction with life, self esteem, and positive affect 
+
+#individuals dimensions 1 and 2
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'IMS_total_bin',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(1,2)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'esteem_bin',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(1,2)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'SWLS_bin',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(1,2)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'rel_understanding_bin',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(1,2)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+
+fviz_mca_ind(mca_bin_model,
+             label = 'none',
+             habillage = 'negative_affect_bin',
+             addEllipses = TRUE, ellipse.type = 'confidence',
+             ggtheme = theme_minimal(),
+             axes = c(1,2)) +
+  scale_color_paletteer_d('MoMAColors::Klein')
+
+#variable importance dimensions 2 and 3
+fviz_mca_var(mca_bin_model, choice = 'var', axes = c(2,3))
+#no overlap in important variables 
+
+#variable importance dimensions 1 and 3
+fviz_mca_var(mca_bin_model, choice = 'var', axes = c(1,3))
+# no overlap in important variables 
+
+#variable importance dimensions 3 and 4
+fviz_mca_var(mca_bin_model, choice = 'var', axes = c(3,4))
+# overlapping variables are gender and sexual orientation
